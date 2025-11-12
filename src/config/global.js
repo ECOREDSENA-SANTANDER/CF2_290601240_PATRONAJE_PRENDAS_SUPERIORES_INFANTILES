b40ee4a4de1eb402c87103f8492395fd5ef2d02a
Andrea Botello <andrea.botellor@misena.edu.co>
2025-11-12 10:17:41 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -108,7 +108,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
